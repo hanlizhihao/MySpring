7e3b45e530fc9c64250e86c622f7a375ec71480e
lizhihan1 <hanlizhi@jd.com>
2017-09-22 17:40:29 +0800

    aop

---
+++ src/main/java/com/spring/myspring/aop/BeforeAdvice.java
@@ -0,0 +1,11 @@
+package com.spring.myspring.aop;
+
+import org.aopalliance.aop.Advice;
+
+/**
+ * @author hanlizhi
+ * @create 2017-09-22 17:09
+ */
+
+public interface BeforeAdvice extends Advice {
+}
